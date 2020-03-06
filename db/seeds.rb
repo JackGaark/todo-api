1c6d69b94711c3676dfe54503c0786d228076375
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2020-03-06 12:43:33 -0500

    todo-react-rails

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Todo.create(title: "Buy food: milk, bread, fruits", done: false)
+Todo.create(title: "Imagine Dragons tickets", done: false)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
